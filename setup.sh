707f7032c23d6b1187393a1f2a25c2344c1c9b0a
Prudhvi Vajja <v.prudhvi0001@gmail.com>
2020-08-10 21:11:58 -0400

    OK Deploy

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"pvajja@iu.edu\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
